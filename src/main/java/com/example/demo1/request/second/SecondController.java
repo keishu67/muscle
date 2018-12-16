100ba2bb29c0fed305510d42227da50321295d61
shin-kinoshita <kinoshita116@gmail.com>
2018-12-16 16:25:01 +0900

    @PathaVarible を使用した書き方を追加

--- src/main/java/com/example/demo1/request/second/SecondController.java
+++ src/main/java/com/example/demo1/request/second/SecondController.java
@@ -2,12 +2,22 @@ package com.example.demo1.request.second;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/secondpage")
 public class SecondController {
-    @GetMapping
-     public String index() { return "second/index.html";}
+    @GetMapping("{id}")
+     public String index(@PathVariable Integer id) {
+        System.out.println(id);
+        return "muscle-intro/" + id.toString();
+//        if (id.equals(2)) {
+//            return "second/index.html";
+//        } else if (id.equals(3)) {
+//            return "third/index.html";
+//        }
+//        return "top/index.html";
+    }
 }
 
