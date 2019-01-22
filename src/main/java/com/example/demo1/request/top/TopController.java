0ff6fbea1b7c1c62e15a0ff616838a9fd382fc7c
keishu67 <rt0023pe@gmail.com>
2019-01-22 23:10:51 +0900

    TopControllerでidに定数をいれて、top/index.htmlのURLパスに渡す。 SecondController, ThirdControllerを消す

--- src/main/java/com/example/demo1/request/top/TopController.java
+++ src/main/java/com/example/demo1/request/top/TopController.java
@@ -1,14 +1,20 @@
 package com.example.demo1.request.top;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mainpage")
 public class TopController {
-    @GetMapping
-    public String index() {
+
+@GetMapping
+
+    public String index(Model model) {
+        model.addAttribute("id", 1);
+
         return "top/index.html";
     }
 }
